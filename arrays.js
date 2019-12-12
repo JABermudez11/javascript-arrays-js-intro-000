28ff75f6b1bd87e4ae90ec890638e74fb278cc10
Justin Bermudez <justinanbermudez@gmail.com>
2019-12-12 07:45:13 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element) {
-  var newArray = array.push(element);
+  var newArray = [...array, element];
   return newArray;
 }
 
